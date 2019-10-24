26766e10416b02c77b97d737f74c1fda3c225409
ilinoa <ilinoa90@gmail.com>
2019-10-25 01:43:39 +0300

    add time for users

--- public/js/index.js
+++ public/js/index.js
@@ -1,40 +1,29 @@
 $(()=>{
-	var time = ''
-	var socket = io.connect() 			// подключаем socket -> connection
-	var $form = $('#messForm') 			// обращаемся к объекту по Id - messForm
-	var $name = $('#name')
+	var socket = io.connect(); 			
+	var $form = $('#messForm'); 			
+	var $name = $('#name');
+	var $input_mess = $('#message'); 	
+	var $all_mess = $('#all_mess');		
+	var $out_mess = $('#out_mess');
+	var $counter = $('#counter');
 	
-	var $input_mess = $('#message') 	// поле ввода
-	var $all_mess = $('#all_mess')		// поле вывода
-	var $out_mess = $('#out_mess')
-	var $counter = $('#counter')
-	/*
-	*	Этот блок содержит соединение при новом подклюючении
-	*	Если его убрать пользователю будет приходить копия сообщений последней сессии при пропадании соединения
-	*/
+
 	socket.on('connect', () => {
 		$('#all_mess').empty();			
-		console.log(socket.connected); // true
+		console.log(socket.connected); 
 	});
-
-	/*
-	*	отслеживаем в форме событие submit
-	*	preventDefault() - предотвращает стандартное поведение
-	*	socket обращается к функции emit() для вызова события - укажет 'send mess' и данные поле ввода
-	*	после отправки сообщения указываем пуcтую строку
-	*/
+	
 	$form.submit((event) => {
-		event.preventDefault()
-	 	time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
-		socket.emit('send mess', {mess:$input_mess.val(), name:$name.val()})
-		$input_mess.val('')
-	})
+		event.preventDefault();
+		socket.emit('send mess', { mess:$input_mess.val(), name:$name.val()});
+		$input_mess.val('');
+	});
 
 	socket.on('add mess', (data) => {
 		$all_mess.prepend(
-			'<div> <k class="small">' +' </k><b> ' + data.name + '</b>: ' + data.mess + '</div><br>'
-		)
-	})
+			'<div> <k class="small">'+ data.time +' </k><b> ' + data.name + '</b>: ' + data.mess + '</div><br>'
+		);
+	});
 	
 	socket.on('data out', (ret, users) => {
 		$counter.empty()
@@ -43,7 +32,7 @@ $(()=>{
 		$out_mess.empty()
 		for (var i=0; i<ret.length; i++)
 		$out_mess.prepend( 
-			'<div> <k class="small">' + ' </k><b> ' + ret[i].name + '</b>: ' + ret[i].message + '</div><br>' 
+			'<div> <k class="small">' + ret[i].time + ' </k><b> ' + ret[i].name + '</b>: ' + ret[i].message + '</div><br>' 
 		)
 	}) 
 })
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = express()
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server)
 var bodyParser = require('body-parser')
-
+var users = []; 
+var connections = [];
+var clients = [];
 app.use('/public', express.static('public'))
 app.set('view engine', 'ejs', 'js')
-
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 server.listen(3000)
 var mysql = require('mysql');
-
+//time: new Date().
 var conn = mysql.createConnection({
   host: 'localhost',
   user: 'admin',
@@ -18,66 +19,48 @@ var conn = mysql.createConnection({
   database: 'project',
   port: 3306
 });
-
 conn.connect();
 app.get('/',  (req, res) => {
 	console.log(req.url)
 	res.render('start', {})	
 });	
-
-
-var clients = [];
-
 io.sockets.on('connect', function(client) {
     console.log(clients.push(client)); 
     client.on('disconnect', function() {
     clients.splice(clients.indexOf(client), 1);
     });
 });
-
-users = []; // хранит всех пользователей с которыми есть соединение
-connections = [] // помещяем сюда все подключения на текущий момент
-/*
-*		io.sockets.on - отслеживает 'connection' и вызывает  function, 
-*		которая содержит объект socket. 
-*/		 
-
 io.sockets.on('connection', (socket) => {	
-	/*
-	* 	.push(socket) - добавление в массив connections
-	*	.splice(connections.indexOf(socket), 1) - находит индекс 
-	*	объекта и удаляет 1 элемент
-	*/
 	users=connections.push(socket);	
 	console.log('connect new user' + users)
 	socket.on('disconnect', (data) => {	
 		--users;	
 		console.log('user disconnected ' + users)
-		
 		connections.splice(connections.indexOf(socket), 1)
 	});
-
 	conn.query("SELECT * FROM chat", (error,result) => {
 		if(error) error;
 		var ret = result;		
-		socket.emit('data out', ret, users);		// исправлено <- io. отправляет ret всем в т ч кто уже получил
-		console.log('TEST'+ret);
+		socket.emit('data out', ret, users);
 	});
-	/*
-	*	когда вызовется 'send mess' -  обратиться к функции emit()
-	*  	'add mess' - передаст mess со значением data  
-	*/
+	
 	socket.on('send mess', (data) => {
 		console.log({ name: data.name, mess: data.mess})
 		io.sockets.emit('add mess', {
 			mess: data.mess, 
-			name: data.name
-		})
+			name: data.name,
+			time: new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")
+		});
 		const name = data.name;
 		const message = data.mess;
-		var query = conn.query("INSERT INTO chat (name, message) VALUES (?,?)", [name, message], (err, data) => {
+		var time = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+		time.toString();
+		console.log('time: '+time)
+		
+		var query = conn.query("INSERT INTO chat (name, message, time) VALUES (?,?,?)", [name, message, time
+		], (err, data) => {
 		  	if(err) return console.log(err);		  
-		})
-	})
-})
+		});
+	});
+});
 console.log('Server run, port 3000')
